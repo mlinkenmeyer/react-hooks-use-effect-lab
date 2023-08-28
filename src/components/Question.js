8f5f7d6bbb40eab6dddbaabc5e97c406704e56a5
Madison <mlinkenmeyer@gmail.com>
2023-08-28 14:51:02 -0700

    setTimeout lab complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    const coundtownTimer = setTimeout(
+      () => setTimeRemaining((timeRemaining) => timeRemaining - 1),
+      1000
+    );
+    return () => clearTimeout();
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -11,6 +23,8 @@ function Question({ question, onAnswered }) {
   }
 
   const { id, prompt, answers, correctIndex } = question;
+
+  console.log("this gets logged before useEffect");
 
   return (
     <>
